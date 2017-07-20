31b119d8450868a0b885a7d696ee8a60cb63d625
Raja <email": null>
2017-07-20 11:28:09 -0400

    Add some controller methods.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      @photo = Photo.find(@comment.photo_id)
+      redirect_to @photo
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @comment = Article.find(params[:id])
+    @comment.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :picture_id)
+  end
 end
