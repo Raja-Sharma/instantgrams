5a2448d4b00884489c0400d054b18b8f1bfa1cdb
Dylan Yang <email": null>
2017-07-20 17:49:22 -0400

    renders json

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,52 +1,57 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = Picture.all
+    picture = Picture.find(1)
+    picture = picture.as_json(include: { comments: {
+      include: :commenter
+      }}
+      )
+    render json: picture
   end
-
-  def show
-    @picture = Picture.find_by(id: params[:id])
-  end
-
-  def new
-    @picture = Picture.new
-  end
-
-  def edit
-
-  end
-
-  def create
-    @picture = Picture.new(picture_params)
-
-    if @picture.save
-      redirect_to @picture
-    else
-      @errors = @picture.errors.full_messages
-      render status: 422, action: :new
-    end
-  end
-
-  def update
-    @picture = Picture.find(params[:id])
-
-    if @picture.update(picture_params)
-      redirect_to @picture
-    else
-      @errors = @picture.errors.full_messages
-      render status: 422, action: :edit
-    end
-  end
-
-  def destroy
-    @picture = Article.find(params[:id])
-    @picture.destroy
-
-    redirect_to picture_path
-  end
-
-  private
-    def picture_params
-      params.require(:picture).permit(:user_id, :, :image, :caption)
-    end
+  #
+  # def show
+  #   @picture = Picture.find_by(id: params[:id])
+  # end
+  #
+  # def new
+  #   @picture = Picture.new
+  # end
+  #
+  # def edit
+  #
+  # end
+  #
+  # def create
+  #   @picture = Picture.new(picture_params)
+  #
+  #   if @picture.save
+  #     redirect_to @picture
+  #   else
+  #     @errors = @picture.errors.full_messages
+  #     render status: 422, action: :new
+  #   end
+  # end
+  #
+  # def update
+  #   @picture = Picture.find(params[:id])
+  #
+  #   if @picture.update(picture_params)
+  #     redirect_to @picture
+  #   else
+  #     @errors = @picture.errors.full_messages
+  #     render status: 422, action: :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @picture = Article.find(params[:id])
+  #   @picture.destroy
+  #
+  #   redirect_to picture_path
+  # end
+  #
+  # private
+  #   def picture_params
+  #     params.require(:picture).permit(:user_id, : :image, :caption)
+  #   end
 
 end
