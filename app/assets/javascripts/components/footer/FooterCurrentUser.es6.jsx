53dbd0c4b91b810c41a1e2988974c967b6d583be
Gus Christensen <augustjc@gmail.com>
2017-07-20 16:40:26 -0400

    Update FooterCurrentUser.es6.jsx

--- app/assets/javascripts/components/footer/FooterCurrentUser.es6.jsx
+++ app/assets/javascripts/components/footer/FooterCurrentUser.es6.jsx
@@ -2,8 +2,7 @@ class FooterCurrentUser extends React.Component {
   render() {
     return(
       <div className="footer-item">
-        <%= link_to "Profile", @user %>
-        # => <a href="/users/<%= @user.id %>">Profile</a>
+        <a href="/users/{user.id}">profile</a>
       </div>
     )
   }
