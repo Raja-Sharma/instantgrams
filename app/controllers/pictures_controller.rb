fb26a6e7557e863fcbc4c1aa7a9b18bcb2fb0c16
Gus Christensen <email": null>
2017-07-20 15:03:14 -0400

    more

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,18 @@
+class PicturesController < ApplicationController
+  def index
+    render "/index"
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+end
