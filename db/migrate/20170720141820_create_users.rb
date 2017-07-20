145941be8c43d64d004c0b4575d5483b1ab09db7
Gus Christensen <email": null>
2017-07-20 10:34:08 -0400

    user model/migration likely complete, iff paperclip?

---
+++ db/migrate/20170720141820_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsersWithAttachments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.attachment :avatar, null: false
+
+      t.timestamps
+    end
+  end
+end
