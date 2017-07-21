b8e34acc0d3e668d6cbd958be9918476a6d6ac2b
gus-christensen <augustjc@gmail.com>
2017-07-21 15:24:59 -0400

    img tags fixed

--- app/assets/javascripts/components/timeline/entry/EntryPicture.es6.jsx
+++ app/assets/javascripts/components/timeline/entry/EntryPicture.es6.jsx
@@ -3,7 +3,7 @@ class EntryPicture extends React.Component {
     let url = this.props.url
     return(
       <div className='entry-picture'>
-        <image src={url} />
+        <img src={url} />
       </div>
     )
   }
